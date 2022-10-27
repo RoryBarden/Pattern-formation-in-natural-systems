b4cfd44119332ea1ce052add08f0eb3614428c70
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 13:05:32 +0100

    Updated reaction time range

--- testing.py
+++ testing.py
@@ -4,4 +4,4 @@ grid = pde.UnitGrid([256, 256])
 state = pde.ScalarField.random_uniform(grid)
 
 eq = pde.DiffusionPDE(diffusivity=0.5)
-result = eq.solve(state, t_range=1).plot()
+result = eq.solve(state, t_range=10).plot()
