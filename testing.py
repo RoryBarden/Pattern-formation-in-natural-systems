7207f892bcff6141e84d07fa77f5368c72fa06ac
BuildTools <unconfigured@null.spigotmc.org>
2022-10-29 22:50:05 +0100

    Implemented a basic version of the reaction-diffusion equation of the latex form (D\nabla^{2}u+r(u)) where D is the diffusivity and r is the reaction kinetics function.

--- testing.py
+++ testing.py
@@ -1,7 +1,14 @@
+import numpy as np
 import pde
 
 grid = pde.UnitGrid([256, 256])
-state = pde.ScalarField.random_uniform(grid)
+state = pde.ScalarField.random_uniform(grid, rng=np.random.default_rng(12345))  # Seedable random state generator
 
-eq = pde.DiffusionPDE(diffusivity=0.5)
-result = eq.solve(state, t_range=10).plot()
+
+def reaction_kinetics(u_value: pde.fields.scalar.ScalarField):
+    return u_value  # Testing with kinetics that do nothing
+
+
+eq = pde.PDE({"u": "diffusivity * laplace(u) + reaction_kinetics(u)"},
+             user_funcs={"reaction_kinetics": reaction_kinetics}, consts={"diffusivity": 0.5})
+result = eq.solve(state, t_range=5).plot()
